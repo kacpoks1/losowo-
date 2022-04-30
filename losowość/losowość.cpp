e71ef7866b6efeecc694d0eef38cfa5473f5bfbe
Admin <Admin@ASUS-TUF-DASH-F>
2022-04-30 15:17:20 +0200

    Add project files.

---
+++ losowość/losowość.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+
+	const int table_size = 20;
+	int table[table_size];
+	char i;
+	char zmienna;
+	int losowa;
+
+
+	for (i = 0; i < table_size; i++)
+	{
+		cout << "wpisz znaki, aby przerwac podaj 0  " << endl;
+
+		cin >> zmienna;
+
+		if (zmienna == '0')
+		{
+			break;
+		}
+		else
+		{
+			table[i] = zmienna;
+		}
+	}
+
+	for (int j = i; j <= i; j++)
+	{
+		losowa = table[i];
+		srand((int)time(0));
+		losowa = rand() % table[i];
+		cout << "losowo" << endl;
+		cout << losowa;
+	}
+
+}
